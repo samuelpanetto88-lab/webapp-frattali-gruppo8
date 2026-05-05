418c902510d2021404ed21852e07ec548c117522
gioiaEuropaUnita <gioia836@istitutoeuropaunita.edu.it>
2026-05-05 16:32:58 +0100

    Update app.py
      adesso si ricorda le impostasioni della ggeneraxione del frattale

--- progetto-frattale/app.py
+++ progetto-frattale/app.py
@@ -1,49 +1,61 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request #flask crea l'istanza dell'applicazione, render_template per mostrare le pagine html e request per gestire i dati inviati dai form
 from fractal.chaos_game import genera_frattale
 import os
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/")             #indirizzamento per la home page
 def home():
     return render_template("index.html")
 
 
-@app.route("/frattale")
+@app.route("/frattale")     #indirizzamento per la pagina del frattale
 def frattale():
     return render_template("frattale.html")
 
 
-@app.route("/generatore", methods=["GET", "POST"])
+@app.route("/generatore", methods=["GET", "POST"])   #indirizzamento per la pagina del generatore
 def generatore():
     immagine = None
     errore = None
+    num_vertici = 3
+    distanza = 0.50
+    iterazioni = 100000
+    colore = "black"
 
     if request.method == "POST":
         try:
-            num_vertici = int(request.form.get("num_vertici", 3))
-            distanza = float(request.form.get("distanza", 0.5))
-            iterazioni = int(request.form.get("iterazioni", 30000))
-            colore = request.form.get("colore", "green")
+            num_vertici = int(request.form.get("num_vertici", num_vertici))
+            distanza = float(request.form.get("distanza", distanza))
+            iterazioni = int(request.form.get("iterazioni", iterazioni))
+            colore = request.form.get("colore", colore)
 
             nome_file = genera_frattale(
                 num_vertici=num_vertici,
                 distanza=distanza,
                 iterazioni=iterazioni,
-                scarta=100,
+                scarta=1000,                
                 colore=colore
             )
 
             immagine = f"generated/{nome_file}"
 
-        except Exception as e:
+        except Exception as e:      #converte lerrore in stringa e lo passa alla pagina html 
             errore = str(e)
 
-    return render_template("generatore.html", immagine=immagine, errore=errore)
+    return render_template(
+        "generatore.html",
+        immagine=immagine,
+        errore=errore,
+        num_vertici=num_vertici,
+        distanza=distanza,
+        iterazioni=iterazioni,
+        colore=colore,
+    )
 
 
-@app.route("/archivio")
+@app.route("/archivio")    #indirizzamento per la pagina dell archivio con tutte le immagini generate  
 def archivio():
     cartella = os.path.join("static", "generated")
     os.makedirs(cartella, exist_ok=True)
@@ -54,5 +66,12 @@ def archivio():
     return render_template("archivio.html", immagini=immagini)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == "__main__":      # verifica se il file è eseguito direttamente e in tal caso avvia l'applicazione Flask
+    app.run(debug=True)     #avvia l'applicazione Flask in modalità debug necessario per lo sviluppo e tolto in produzione
+
+
+# __name__ === "__main__" è una convenzione in Python che indica che il codice all'interno di questo blocco verrà eseguito
+#  solo se il file viene eseguito direttamente, e non quando viene importato come modulo in un altro file. 
+# In questo caso, se app.py viene eseguito direttamente, l'applicazione Flask verrà avviata. 
+# Se invece app.py viene importato in un altro file, il codice all'interno di questo blocco non verrà eseguito, 
+# evitando così di avviare l'applicazione Flask in modo indesiderato.
